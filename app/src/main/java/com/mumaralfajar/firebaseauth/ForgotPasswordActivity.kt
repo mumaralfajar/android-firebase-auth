965f028c6232ff922026bab122b3c69da629b6ef
mumaralfajar <kuhaaku@gmail.com>
2022-07-03 18:04:07 +0700

    forgot password

--- app/src/main/java/com/mumaralfajar/firebaseauth/ForgotPasswordActivity.kt
+++ app/src/main/java/com/mumaralfajar/firebaseauth/ForgotPasswordActivity.kt
@@ -1,8 +1,11 @@
 package com.mumaralfajar.firebaseauth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_forgot_password.*
 
 class ForgotPasswordActivity : AppCompatActivity() {
@@ -13,12 +16,48 @@ class ForgotPasswordActivity : AppCompatActivity() {
         initActionBar()
 
         btnSendEmail.setOnClickListener {
-            Toast.makeText(this, "Send Email", Toast.LENGTH_SHORT).show()
+            val email : String = etEmailForgotPass.text.toString().trim()
+            if (email.isEmpty()) {
+                etEmailForgotPass.error = "Please fill your email"
+                etEmailForgotPass.requestFocus()
+                return@setOnClickListener
+            } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                etEmailForgotPass.error = "Please input a valid email"
+                etEmailForgotPass.requestFocus()
+                return@setOnClickListener
+            } else {
+                forgotPass(email)
+            }
         }
 
         tbForgotPass.setNavigationOnClickListener {
             finish()
         }
+    }
+
+    private fun forgotPass(email: String) {
+        FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        this,
+                        "Your reset password has been sent to your email",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    startActivity(Intent(this, SignInActivity::class.java))
+                    finishAffinity()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Failed to reset password",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+            .addOnFailureListener {
+                exception ->
+                Toast.makeText(this, exception.message, Toast.LENGTH_SHORT).show()
+            }
     }
 
     private fun initActionBar() {
--- app/src/main/java/com/mumaralfajar/firebaseauth/SignInActivity.kt
+++ app/src/main/java/com/mumaralfajar/firebaseauth/SignInActivity.kt
@@ -3,17 +3,32 @@ package com.mumaralfajar.firebaseauth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.EmailAuthProvider
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_in.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
         initActionBar()
+        initFirebaseAuth()
 
         btnSignIn.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+            val email : String = etEmailSignIn.text.toString().trim()
+            val pass : String = etPasswordSignIn.text.toString().trim()
+
+            if (checkValidation(email, pass)) {
+                loginToServer(email, pass)
+            }
         }
 
         btnForgotPass.setOnClickListener {
@@ -23,6 +38,49 @@ class SignInActivity : AppCompatActivity() {
         tbSignIn.setNavigationOnClickListener {
             finish()
         }
+    }
+
+    private fun loginToServer(email: String, pass: String) {
+        val credential = EmailAuthProvider.getCredential(email, pass)
+        firebaseAuth(credential)
+    }
+
+    private fun firebaseAuth(credential: AuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                CustomDialog.hideLoading()
+                if (task.isSuccessful) {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finishAffinity()
+                } else {
+                    Toast.makeText(this, "Sign In Failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+            .addOnFailureListener { exception ->
+                CustomDialog.hideLoading()
+                Toast.makeText(this, exception.message, Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun checkValidation(email: String, pass: String): Boolean {
+        if (email.isEmpty()) {
+            etEmailSignIn.error = "Please fill your email"
+            etEmailSignIn.requestFocus()
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            etEmailSignIn.error = "Please input a valid email"
+            etEmailSignIn.requestFocus()
+        } else if (pass.isEmpty()) {
+            etPasswordSignIn.error = "Please fill your password"
+            etPasswordSignIn.requestFocus()
+        } else {
+            return true
+        }
+        CustomDialog.hideLoading()
+        return false
+    }
+
+    private fun initFirebaseAuth() {
+        auth = FirebaseAuth.getInstance()
     }
 
     private fun initActionBar() {
